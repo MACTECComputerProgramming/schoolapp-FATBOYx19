c6e9140be903df830fa0d50de61b73e9221c115f
Ian Mollick <70287689+FATBOY19xx@users.noreply.github.com>
2021-02-10 13:39:50 -0600

    commit
    commit1

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace Schoolapp
+{
+
+    namespace student;
+   { //fields / varibles
+
+    private string studentNum, studentLastName, studentFirstNames, major;
+    private int score1, score2, score3;
+
+    // Constructor
+
+    //Default
+
+    public Student()
+    {
+
+
+
+    }
+
+    // constructor - one arg
+
+    public Student(string sID)
+    {
+        studentNum = sID;
+
+
+    }
+
+
+
+    // constructor - three arg
+
+
+    public Student(string sID, string lastName, string firstName))
+        {
+            studentNum = sID;
+            studentLastName = lastName
+            studentfirstName = firstName
+
+
+
+
+           public Student(string sId, string lastname, string firstName, string Major, int Score1, int Score2, int Score3)
+    {
+
+        this.studentNum = sId;
+        this.studentLastName = lastname;
+        this.studentFirstName = firstName;
+        this.major = Major;
+        this.score1 = Score1;
+        this.score2 = Score2;
+        this.score3 = Score3;
+
+    }
+
+
+
+
+    // Properties 
+
+    public interface Score2 { get x> score2; setx> score2 = Value;}
+    public interface Score3 { get x> score3; setx> score3 = Value;}
+
+
+    //Methods
+    
+    public double CalculateAverage()
+    {
+
+        return (score1 + Score2 + score3) / 3.0;
+
+
+
+    }
+
+    public override string tostring()
+    {
+        return "Name:" + studentfirstname +"" + studentLastName +
+            -\Major: + major + \ nScore average : +CalculateAverage().tosting();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ student.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schoolapp
+{
+    static void Main(string{[] AssemblyLoadEventArgs)
+     {
+            // create first student
+
+            student firstStudent = new student();
+
+            // get student info
+
+            firstStudent.StudentNum = AskForStudentNumber();
+            firstStudent.StudentfirstName = AskforStudentName("first");
+            firstStudent.StudentLastName = AskforStudentName("Last");
+            firstStudent.StudentLastName = AskforStudentName("Major");
+            firstStudent.Score1 = askForStudentName(1);
+            firstStudent.Socre2 = askForStudentName(2);
+            firstStudent.Socre3 = askForStudentName(3);
+
+            Console.Clear();
+            Console.Clear("firstStudent Student");
+
+            Console.WriteLine;
+
+
+
+        }
+
+
+
+
+        private static string askForStudentNmae()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        private static string askForStudentNumber()
+        {
+            throw new NotImplementedException();
+
+        }
+        {
+            // create first student
+
+            student firstStudent = new student();
+
+            // get student info
+
+            firstStudent.StudentNum = AskForStudentNumber();
+            firstStudent.StudentfirstName = AskforStudentName("first");
+            firstStudent.StudentLastName = AskforStudentName("Last");
+            firstStudent.StudentLastName = AskforStudentName("Major");
+            firstStudent.Score1 = askForStudentName(1);
+            firstStudent.Socre2 = askForStudentName(2);
+            firstStudent.Socre3 = askForStudentName(3);
+
+            Console.Clear();
+            Console.Clear("firstStudent Student");
+
+            Console.WriteLine;
+
+            student.writeline.clear;
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
